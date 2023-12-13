55056bc265ba524e4a1830d04fa81c0ed0179797
setiawan2023 <151593560+setiawan2023@users.noreply.github.com>
2023-12-13 16:08:12 +0700

    Add files via upload
    Hasil Ujian Agung,Alya,Hasbi

---
+++ balok agung.cpp
@@ -0,0 +1,31 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+float fbalok(int p,int l,int t){
+    return p*l*t;
+}
+int main(){
+    int p,l,t;
+    float hasil;
+    cout<<"========================="<<endl;
+    cout<<"# Program mencari volume #"<<endl;
+    cout<<"# Program Di Buat By Agung#"<<endl;
+    cout<<"========================="<<endl;
+    cout<<"Pilihlah program Yang di inginkan"<<endl;
+     cout<<"Menghitung volume balok"<<endl;
+    }
+      if(pilih == 1){
+        cout<<"menghitung volume balok "<<endl;
+        cout<<"masukkan nilai panjang : ";
+        cin>>a;
+        cout<<"masukkan nilai lebar : ";
+        cin>>l;
+        cout<<"masukkan nilai tinggi : ";
+        cin>>t;
+        hasil = fvolumebalok (p,l,t);
+          cout<<"volumebalok : "<<hasil<<endl;
+    }else{
+  system("PAUSE");
+  return 0;
+}
